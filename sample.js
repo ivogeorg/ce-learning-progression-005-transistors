16ac3eb98db86893fd6d16627449c8d7e833b9e6
Ivo Georgiev <ivogeorg@gmail.com>
2020-02-22 14:53:12 -0700

    Transferred with submission template

---
+++ sample.js
@@ -0,0 +1,5 @@
+// File: sample.js
+// Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+// Author: Ivo Georgiev (igeorgi1@msudenver.edu)
+// Last edit: 2019-11-18
+
